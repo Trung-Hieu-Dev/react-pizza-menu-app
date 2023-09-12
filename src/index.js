f64fcd5ddfb9ba4268e4abe785fe2e25ea3f89bb
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-09-12 22:49:46 +0700

    Rendering List

--- src/index.js
+++ src/index.js
@@ -72,32 +72,40 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza
-        name="Pizza Salamino"
-        ingredients="Tomato, mozarella, and pepperoni"
-        price={15}
-        photoName="pizzas/salamino.jpg"
-      />
-      <Pizza
-        name="Pizza Prosciutto"
-        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
-        price={18}
-        photoName="pizzas/prosciutto.jpg"
-      />
+
+      {/* Rendering List */}
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza key={pizza.name} pizzaObj={pizza} />
+        ))}
+      </ul>
+
+      {/* <Pizza */}
+      {/*   name="Pizza Salamino" */}
+      {/*   ingredients="Tomato, mozarella, and pepperoni" */}
+      {/*   price={15} */}
+      {/*   photoName="pizzas/salamino.jpg" */}
+      {/* /> */}
+      {/* <Pizza */}
+      {/*   name="Pizza Prosciutto" */}
+      {/*   ingredients="Tomato, mozarella, ham, aragula, and burrata cheese" */}
+      {/*   price={18} */}
+      {/*   photoName="pizzas/prosciutto.jpg" */}
+      {/* /> */}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price + 3}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
